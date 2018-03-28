3a65d9dfedc33388cd1c528a89dd7719ba4dc105
Deniro12 <37781522+Deniro12@users.noreply.github.com>
2018-03-28 21:13:14 +0300

    Add files via upload

---
+++ hw3 part2.py
@@ -0,0 +1,17 @@
+import sys
+card_number = input("Please enter card number:")
+mm_yy = input("Please enter mm/yy:" )
+CVV = input ("Please enter CVV:" )
+try:
+    var = mon1 == int(mon)
+except: #ValueError:
+    print ("OK")
+if len(card_number) != 16:
+    sys.exit(1)
+if len(CVV)  < 3:
+    sys.exit("Error!!!")
+if int(card_number) == 1234567812345678:
+            print ("Ha-ha-ha.Now I will use your credit card!")
+
+
+
