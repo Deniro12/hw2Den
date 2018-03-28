4ed76e8446e692ec78835dc71b46d82464f6dbee
Deniro12 <37781522+Deniro12@users.noreply.github.com>
2018-03-28 23:07:31 +0300

    Update hw3 part2.py

--- hw3 part2.py
+++ hw3 part2.py
@@ -2,6 +2,7 @@ import sys
 card_number = input("Please enter card number:")
 mm_yy = input("Please enter mm/yy:" )
 CVV = input ("Please enter CVV:" )
+mm_yy1 == int(mm_yy)
 try:
     var = mm_yy1 == int(mm_yy)
 except: #ValueError:
